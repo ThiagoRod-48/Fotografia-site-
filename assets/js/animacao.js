6725dc2a930a5cea003ffb6591bc92efeb175b48
ThiagoRod-48 <thiago.rodrigo73@yahoo>
2026-01-12 20:12:16 -0300

    Teve muitos ajustes e modificações gerais + remodelação do arquivo responsivo-elementos.scss

--- assets/js/animacao.js
+++ assets/js/animacao.js
@@ -1,11 +1,27 @@
-gsap.from(".polaroide", {
-  y: -35,
-  autoAlpha: 0,
-  rotation: -15,
-  duration: 0.6,
-  stagger: 0.15,
-  ease: "back.out(1.7)",
-  onComplete: () => {
-    atualizarZIndex();
+gsap.fromTo(
+  ".polaroide",
+  {
+    autoAlpha: 0,
+    y: 30,
+    rotation: 0,
+    x: 0,
   },
-});
+  {
+    autoAlpha: 1,
+    y: 0,
+    duration: 0.6,
+    stagger: 0.15,
+    ease: "back.out(1.7)",
+    x: (i) => {
+      if (i === 0) return -30;
+      if (i === 2) return 30;
+      return 0;
+    },
+    rotation: (i) => {
+      if (i === 0) return -6;
+      if (i === 2) return 6;
+      return 0;
+    },
+    onComplete: () => atualizarZIndex(),
+  }
+);
